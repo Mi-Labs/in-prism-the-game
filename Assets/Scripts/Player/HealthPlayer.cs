ce3910e1d26bab8cab52a4bdc8ff0f2dcec14681
Micha <mi.labs1695@googlemail.com>
2018-05-02 09:09:10 +0200

    Implement Invulnerability

--- Assets/Scripts/Player/HealthPlayer.cs
+++ Assets/Scripts/Player/HealthPlayer.cs
@@ -11,11 +11,17 @@ public class HealthPlayer : MonoBehaviour {
     // maximal healthpoints of the player
     private const int maxlifepoints = 100;
 
+    // Holds the status of invulnerability
+    private bool isInvulnerable;
+
 	// Use this for initialization
 	void Start ()
     {
         // On Start the lifepoints should be at maximum
         lifepoints = maxlifepoints;
+
+        // Set isInvulnerable to false -> only true when powerup is active
+        isInvulnerable = false;
 	}
 
     /// <summary>
@@ -24,28 +30,28 @@ public class HealthPlayer : MonoBehaviour {
     /// <param name="damage">The amount of lifepoints, that should be decreased</param>
     public void DecreaseLife(int damage)
     {
-        //if unvenerablity -> do nothing
-        //else this
+        if(!isInvulnerable)
+        {
+            //Debug.Log("Damage " + damage);
 
-        //Debug.Log("Damage " + damage);
+            // Calculate the lifepoints, that are left after the decrease
+            lifepoints = lifepoints - damage;
 
-        // Calculate the lifepoints, that are left after the decrease
-        lifepoints = lifepoints - damage;
+            //Debug.Log("LifeP" + lifepoints);
 
-        //Debug.Log("LifeP" + lifepoints);
+            // Change the text on the lifebar
+            ChangeLifeBar();
 
-        // Change the text on the lifebar
-        ChangeLifeBar();
-        
-        // If the lifepoints are below zero, reset this scene
-       if(lifepoints <= 0)
-       {
-            // Get the buildindex of the active scence
-            int activescene = SceneManager.GetActiveScene().buildIndex;
-        
-            // Reload the scence
-            SceneManager.LoadScene(activescene);
-       }
+            // If the lifepoints are below zero, reset this scene
+            if (lifepoints <= 0)
+            {
+                // Get the buildindex of the active scence
+                int activescene = SceneManager.GetActiveScene().buildIndex;
+
+                // Reload the scence
+                SceneManager.LoadScene(activescene);
+            }
+        }   
     }
 
     /// <summary>
@@ -92,5 +98,21 @@ public class HealthPlayer : MonoBehaviour {
     {
         // Get the ReadPlayerTime script and change the value of the lifebar to the actual value
         this.GetComponent<ReadPlayerTime>().ChangeTextValue();
+    }
+
+    /// <summary>
+    /// This method toggles the status of Invulnerability
+    /// </summary>
+    public void ToogleIsInvulnerable(bool status)
+    {
+        // If the powerUp Invulnerability is active
+        if(status == true)
+        {
+            isInvulnerable = true;
+        }
+        else
+        {
+            isInvulnerable = false;
+        }
     }
 }
--- Assets/Scripts/PowerUp/Invulnerablity.cs
+++ Assets/Scripts/PowerUp/Invulnerablity.cs
@@ -4,38 +4,147 @@ using UnityEngine;
 
 public class Invulnerablity : MonoBehaviour, IPowerUp {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+    /* Variables */
+
+
+    // Hold the time, that this powerUp is active
+    private int activetime;
+
+    // Hold the time that this powerUp needs to cool down
+    private int cooldowntime;
+
+    /* Hold the status of this PowerUP
+     * true = player can use this powerUp
+     * false = player can't use this powerUp */
+    private bool isAvailable;
+
+    /* Hold the status of cooldown
+     * true = Cooldown is active 
+     * false = Cooldown is not active */
+    private bool cooldownIsActive;
+
+    // Holds the preFab timer
+    private GameObject timer;
+
+    // Holds the player object
+    private GameObject player;
+
+
+
+    /* Methods */
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Get the world config, with data for this powerup
+        World_Config config = GameObject.Find("World_Config").GetComponent<World_Config>();
+
+        // Set activetime
+        activetime = config.activetime_jump;
+
+        // Set cooldowntime
+        cooldowntime = config.cooldowntime_jump;
+
+        // Set cooldown-phase for this PowerUp to false (no active cooldown)
+        cooldownIsActive = false;
+
+        // Set is_available // Currently for tests -> normal activation trough powerupmenu
+        isAvailable = true;
+
+        // Find the player and assign it to player
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Get the prefab-timer
+        timer = config.timer;
+    }
+
+
+    /// <summary>
+    ///  Cooldown-Mechanic. Starts the spezific cooldown
+    /// </summary>
     public void StartCooldown()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Cooldown has started");
+
+        // Toogle invulnerabilty to false
+        player.GetComponent<HealthPlayer>().ToogleIsInvulnerable(false);
+
+        // Start Cooldown-Timer
+        StartTimer(cooldowntime);
+
+        // Set cooldownphase to not active
+        cooldownIsActive = false;
     }
 
+    /// <summary>
+    /// This method is called to start the powerUp, if available
+    /// </summary>
     public void StartPowerUp()
     {
-        throw new System.NotImplementedException();
+        if (isAvailable)
+        {
+            GameObject.Find("PowerUpMenu").GetComponent<PowerUp_Menu>().ToogleMenu();
+
+            PowerUpAction();
+        }
+        else
+        {
+            // Debug.Log("PowerUp is not ready");
+        }
     }
 
+    /// <summary>
+    /// This method start the action of the powerUp (also a countdown)
+    /// </summary>
     public void PowerUpAction()
     {
-        throw new System.NotImplementedException();
+        //Debug.Log("PowerUpAction started");
+
+        // Start timer with the active power up time
+        StartTimer(activetime);
+
+        // Toggle the invulnerablity in the health script
+        player.GetComponent<HealthPlayer>().ToogleIsInvulnerable(true);
+
+        // Set cooldown phase to active
+        cooldownIsActive = true;
     }
 
+    /// <summary>
+    ///  This method starts a timer with an spezific time to run
+    /// </summary>
+    /// <param name="time">Active Time of the timer, in Seconds</param>
     public void StartTimer(int time)
     {
-        throw new System.NotImplementedException();
+        // Generate a cloned timer as GameObject
+        GameObject timerClone = Instantiate(timer, this.transform.position, Quaternion.identity, this.transform) as GameObject;
+
+        // Start the timer with the given parameter
+        timerClone.SendMessage("StartTimer", time);
+
+        //Debug.Log("Started Timer with " + time + " seconds");
     }
 
+    /// <summary>
+    ///  If the time of the timer is up, then this method should be called.
+    ///  If the powerup was used, then the cooldown starts and you should access this powerUp
+    /// </summary>
     public void TimeIsUp()
     {
-        throw new System.NotImplementedException();
+        if (cooldownIsActive)
+        {
+            // Start the cooldown timer
+            StartCooldown();
+
+            // Make the powerUp unavailable for the player
+            isAvailable = false;
+        }
+        else
+        {
+            isAvailable = true;
+            // Debug.Log("Boost is ready again");
+        }
     }
 }
