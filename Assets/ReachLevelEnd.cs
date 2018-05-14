b247d535be900d513524546b7dafe9cb782b5731
gitttigit <nico42geiger@gmail.com>
2018-05-14 13:58:17 +0200

    Add an end
    Trigger at the end and load a picture

---
+++ Assets/ReachLevelEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachLevelEnd : MonoBehaviour {
+    public GameObject pictureText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("LädText");
+        Instantiate(pictureText, this.transform.position, Quaternion.identity, transform);
+    }
+}
