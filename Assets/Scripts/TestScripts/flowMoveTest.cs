1869eea0382385f764422cedcdc55914f937e218
gitttigit <nico42geiger@gmail.com>
2018-05-14 11:54:25 +0200

    CameraMovement
    The Camera follows only until the level bounds

---
+++ Assets/Scripts/TestScripts/flowMoveTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flowMoveTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(Camera.main.gameObject);
+        Camera.main.gameObject.GetComponent<CameraFlow>().enabled=true; //aktivates the smooth camera follow
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
