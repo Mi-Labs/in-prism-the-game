affca22d1a0fb5d534db0a734ad5add6870fd863
Unknown <mi.labs1695@googlemail.com>
2018-05-24 14:02:39 +0200

    Implementing Sticky & Second Layer Grass
    - Grass is added on every Underground-Object (buggy)
    - Sticky (not implemented in correct script)

--- Assets/Scripts/PowerUp/StickyForPlayer.cs
+++ Assets/Scripts/PowerUp/StickyForPlayer.cs
@@ -6,35 +6,81 @@ public class StickyForPlayer : MonoBehaviour {
 
     private GameObject m_player;
 
-	// Use this for initialization
-	void Start ()
+    private float m_fakeGravity = 9.8f;
+
+    public static ConstantForce2D stickyGravity = null;
+
+
+    private void Awake()
     {
-        m_player = this.gameObject;	
-	}
-	
+        m_player = this.gameObject;
+
+        if (stickyGravity == null)
+        {
+           stickyGravity = m_player.AddComponent<ConstantForce2D>();
+        }
+    }
 
 
     private void OnCollisionEnter2D(Collision2D _collision)
     {
+        stickyGravity.enabled = true;
+
         Transform otherCollider = _collision.gameObject.transform;
 
-        //  Debug.Log(this.transform.position);
+        // Debug.Log(this.transform.position);
         // Debug.Log(otherCollider.position);
         Vector2 stickyDirection = (Vector2)otherCollider.position - (Vector2)transform.position;
-         Debug.Log(stickyDirection);
+        Debug.Log(stickyDirection);
 
-        float StickyDirectionX = stickyDirection.x;
+        float stickyDirectionX = stickyDirection.x;
+        float stickyDirectionY = stickyDirection.y;
+
+        bool isInXDirection = CheckForXDirection(stickyDirection);
 
         m_player.GetComponent<Rigidbody2D>().gravityScale = 0;
 
-        ConstantForce2D stickyGravity = m_player.GetComponent<ConstantForce2D>();
 
-        if(stickyGravity == null)
+        if (isInXDirection)
         {
-            m_player.AddComponent<ConstantForce2D>().force = new Vector2(0.0f, -9.8f);
+            if(stickyDirectionX < 0.0f)
+            {
+                stickyGravity.force = new Vector2(-m_fakeGravity, 0.0f);
+            }
+            else
+            {
+                stickyGravity.force = new Vector2(m_fakeGravity, 0.0f);
+            }
         }
-        
-        
+        else
+        {
+            if(stickyDirectionY < 0.0f)
+            {
+                stickyGravity.force = new Vector2(0.0f, -m_fakeGravity);
+            }
+            else
+            {
+                stickyGravity.force = new Vector2(0.0f, m_fakeGravity);
+            }
+        }
+
+    }
+
+    private bool CheckForXDirection(Vector2 _collisionDirection)
+    {
+        if (Mathf.Abs(_collisionDirection.x) > Mathf.Abs(_collisionDirection.y))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D _collision)
+    {
+        stickyGravity.force = new Vector2(0.0f, -m_fakeGravity);
     }
 
 }
---
+++ Assets/Scripts/PreFab Scripts/AddTopping.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTopping : MonoBehaviour {
+
+    public GameObject m_topping;
+
+    private Vector2 m_castDirection;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        m_castDirection = new Vector2(0.0f, -1.0f);
+
+        RaycastHit2D hit = Physics2D.CircleCast(this.gameObject.transform.position, 1.0f, m_castDirection);
+
+        if(hit.collider.gameObject.tag.Equals("Underground"))
+        {
+            Vector2 grounddirection=hit.transform.position - this.transform.position;
+            // Debug.Log("Ground below me");
+
+           if(!(grounddirection.y <0.0f))
+            {
+                Instantiate(m_topping, this.transform.position, Quaternion.identity, this.transform);
+            }
+            
+        }
+    
+	}
+	
+
+}
