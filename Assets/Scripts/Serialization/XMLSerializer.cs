96c7e6c4c223f584a23ceda7fce76a6e85584403
Unknown <mi.labs1695@googlemail.com>
2018-07-04 16:51:18 +0200

    Starting Serialization

---
+++ Assets/Scripts/Serialization/XMLSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+public class SerializerXML
+{
+    public static void Serialize(object _item, System.String _path)
+    {
+        // Create a new XMLserializer with the given object
+        XmlSerializer serializer = new XmlSerializer(_item.GetType());
+        // Create a new stream writer at the given path
+        StreamWriter writer = new StreamWriter(_path);
+        // Serialize the object on a new file created on this path
+        serializer.Serialize(writer.BaseStream, _item);
+        // Close the file and the writer
+        writer.Close();
+    }
+}
