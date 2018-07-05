cdcc5ad9cc09a80113723ec56ecc8d2eb319e5c9
Micha <mi.labs1695@googlemail.com>
2018-07-05 14:27:27 +0200

    Working on Serialization II
    - Added JSON Serializer

---
+++ Assets/LevelFinish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFinish : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched the end");
+        WorldLevelGO m_SaveGame = GameObject.FindGameObjectWithTag("LevelSave").GetComponent<WorldLevelGO>();
+
+        m_SaveGame.m_IsSaving = true;
+
+        JSONSerializer serializer = GameObject.FindGameObjectWithTag("JSONSerialize").GetComponent<JSONSerializer>();
+
+        serializer.Serialize();
+    }
+}
---
+++ Assets/Scripts/Serialization/JSONSerializer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class JSONSerializer : MonoBehaviour {
+
+    public string m_filePath;
+
+    public WorldLevelGO m_gameSave;
+
+    public void Awake()
+    {
+        m_filePath = Application.persistentDataPath +"savegame.json";
+        m_gameSave = GameObject.FindGameObjectWithTag("LevelSave").GetComponent<WorldLevelGO>();  
+    }
+
+    public void Serialize()
+    {
+        string jsonString = JsonUtility.ToJson(m_gameSave, true);
+        File.WriteAllText(m_filePath, jsonString);
+        Debug.Log("File serialized");
+    }
+
+    public void DeSerialize()
+    {
+        string jsonString = File.ReadAllText(m_filePath);
+        m_gameSave = JsonUtility.FromJson<WorldLevelGO>(jsonString);
+    }
+}
